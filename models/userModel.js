d7288a782425e31df6fbb8d31aa34cd49b84b096
Ankitha P <anki202666@gmail.com>
2021-02-04 12:00:34 +0530

    added db connection and userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,56 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/userController');
+const mongoose = require('mongoose')
+
+
+const UserSchema = mongoose.Schema({
+
+      name: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+      },
+      password: {
+        type: String,
+        required: true, 
+        minLength: 6,
+      },
+      profilePicture: {
+        type: String,
+        required: true, 
+        minLength: 6,
+
+      },
+      phoneNumber:{
+          type:Number,
+          required:true,
+          min:10,
+          max:10
+
+      },
+      skills:{
+          type:[String]
+
+      },
+
+      role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+      },
+      active: {
+        type: Boolean,
+        default: true,
+        select: false,
+      },
+      description:{
+          type:String
+      }
+    });
+
+
+module.exports = mongoose.model('user', UserSchema);
+    
