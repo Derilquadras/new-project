e1e93ddbde41e2455f41d76738fbb28fb02d8eb9
Ankitha P <anki202666@gmail.com>
2021-02-04 13:57:52 +0530

    getAll route

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv')
 const port = process.env.PORT || 3000;
+
+dotenv.config();
+
 
 const connection = require('./connection')
 const userRoutes = require('./routes/userRoutes');
 
 
 app.use('/api/users', userRoutes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.use((req, res)=> {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+  });
+
+
+
+  app.listen(port, () => console.log(`listening on port ${port}`));
