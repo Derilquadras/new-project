655daee9fe630c8cbcf6d83122dcc5e84c4716c1
Derilquadras <derilquadras@gmail.com>
2021-02-08 23:07:38 +0530

    make it better

--- connection.js
+++ connection.js
@@ -1,19 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const database = process.env.MONGODB_URL || 'mongodb://localhost/newDB'
+const database = process.env.MONGODB_URL;
 
 const option = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-}
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
 
-const connection = mongoose.connect(database, option).then(con => {
-    console.log('DB connection Successfully!');
-}).catch(err => { // if error we will be here
-    console.error('App starting error:', err.stack);
+const connection = mongoose
+  .connect(database, option)
+  .then((con) => {
+    console.log("DB connection Successfully!");
+  })
+  .catch((err) => {
+    // if error we will be here
+    console.error("App starting error:", err.stack);
     process.exit(1);
-});
+  });
 
 module.exports = connection;
